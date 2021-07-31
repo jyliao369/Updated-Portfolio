d117e65ac9300ce72c990b298f80378c3d9fb014
jyliao369 <jyliao369@gmail.com>
2021-07-31 17:45:55 -0400

    made big changes to html and overall style of portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+var buttonEL = $('#btn');
+
+buttonEL.on('click', function () {
+    console.log("It works");
+    const testitem = document.createElement("div");
+
+    testItem.setAttribute("")
+
+  });
+  
